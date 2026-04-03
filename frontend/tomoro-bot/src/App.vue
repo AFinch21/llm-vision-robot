19473ef37d76d0612639271a45969fd3cf9c685f
Andrew Finch <andrew.finch74@gmail.com>
2026-04-03 19:31:57 +0100

    fix/adds frontend and backend folders, cleans up

---
+++ frontend/tomoro-bot/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SiteNav from './components/SiteNav.vue'
+</script>
+
+<template>
+  <div class="app-shell">
+    <header class="site-header">
+      <div class="site-header-inner">
+        <h1 class="site-title">TomoroBot Control Panel</h1>
+
+        <SiteNav />
+      </div>
+    </header>
+
+    <main class="page-content">
+      <RouterView />
+    </main>
+  </div>
+</template>
---
+++ frontend/tomoro-bot/src/components/LikeCounter.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  likes: Number,
+  dislikes: Number,
+})
+
+const emit = defineEmits(['like', 'dislike', 'reset'])
+</script>
+
+<template>
+  <h1>👍 Likes: {{ props.likes }}</h1>
+  <h1>👎 Dislikes: {{ props.dislikes }}</h1>
+  <button @click="emit('like')">Like</button>
+  <button @click="emit('dislike')">Dislike</button>
+  <button @click="emit('reset')">Reset</button>
+</template>

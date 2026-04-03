5ecd91924b2382dc5c7bace9c55be8e723650697
Andrew Finch <andrew.finch74@gmail.com>
2026-04-03 19:36:30 +0100

    refactor/move app into frontend/tomoro-bot and backend structure

--- frontend/my-app/src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-      <h1>What is this website for?</h1>
-      <p>This webapp grants control of the TomoroBot robot.</p>
-      <p>On Robot Contorls - you will find a live video feed of the robot's camera, and buttons to control the robot's movement.</p>
-      <p>On Likes - you will find a like counter for the robot.</p>
-      <p>On About - you will find information about the robot and the website.</p>
-    </div>
-  </template>
